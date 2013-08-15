a5d72f1e5b9f613150ec482da537f274e9faf714
Takamatsu Makoto <tkmtmkt@gmail.com>
2013-08-15 21:28:32 +0900

    sbt起動時のシステムプロパティ設定変更
    Windows上でsbt-statsプラグインを実行すると日本語を含むファイルが
    処理できなかったが、ソースコードを開く際にデフォルトエンコーディング
    を使用していることが原因であると考えられるため、システムプロパティ
    設定でUTF8を指定するようにした。
    
    上記設定変更により、コンパイル時の警告／エラーメッセージが文字化け
    するため、システムプロパティで言語環境をenを指定するようにした。

--- sbtl.bat
+++ sbtl.bat
@@ -4,6 +4,8 @@
 set SBT_OPTS=-Xms1024M -Xmx1024M -Xss1M
 set SBT_OPTS=%SBT_OPTS% -XX:MaxPermSize=200M -XX:ReservedCodeCacheSize=60M
 set SBT_OPTS=%SBT_OPTS% -XX:+CMSClassUnloadingEnabled -XX:-UseGCOverheadLimit
+set SBT_OPTS=%SBT_OPTS% -Duser.language=en
+set SBT_OPTS=%SBT_OPTS% -Dfile.encoding=UTF-8
 set SBT_OPTS=%SBT_OPTS% -Dsbt.ivy.home="%SCRIPT_DIR%sbt\repository"
 set SBT_OPTS=%SBT_OPTS% -Dsbt.boot.directory="%SCRIPT_DIR%sbt\boot"
 
