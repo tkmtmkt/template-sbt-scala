8f3486257306607d75fe4d55d0cf46c7df5cedf5
Takamatsu Makoto <tkmtmkt@gmail.com>
2016-04-20 20:29:09 +0900

    change sbt script for java8 from java7

--- sbtl.bat
+++ sbtl.bat
@@ -2,7 +2,7 @@
 @set SCRIPT_DIR=%~dp0
 
 set SBT_OPTS=-Xms1024M -Xmx1024M -Xss1M
-set SBT_OPTS=%SBT_OPTS% -XX:MaxPermSize=200M -XX:ReservedCodeCacheSize=60M
+set SBT_OPTS=%SBT_OPTS% -XX:MaxMetaspaceSize=200M -XX:ReservedCodeCacheSize=60M
 set SBT_OPTS=%SBT_OPTS% -XX:+CMSClassUnloadingEnabled -XX:-UseGCOverheadLimit
 set SBT_OPTS=%SBT_OPTS% -Dsbt.global.base="%SCRIPT_DIR%sbt"
 set SBT_OPTS=%SBT_OPTS% -Dsbt.ivy.home="%SCRIPT_DIR%sbt\repository"
